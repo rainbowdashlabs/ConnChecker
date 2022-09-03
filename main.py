8da8e499471aac3497be95ebf882b1bc5df2dadd
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-09-03 15:27:11 +0200

    migrate to aiohttp

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ for arg in sys.argv[1:]:
 
 logger = logger.log()
 
-url = options.get("url", "https://google.com")
+urls:list[str] = options.get("urls", "https://google.com,https://1.1.1.1/,https://amazon.com/,https://atlassian.net,https://github.com/,https://www.youtube.com/,https://www.nvidia.com,https://www.intel.com").split(",")
 timeout = float(options.get("timeout", 3))
 threads = int(options.get("speedcheck_threads", multiprocessing.cpu_count() / 2))
 interval = int(options.get("interval", 5))
 
-logger.info(f"Url: {url} | Timeout: {timeout} | Speedtest Threads: {threads}")
+logger.info(f"Url: {urls} | Timeout: {timeout} | Speedtest Threads: {threads}")
 
 db = Database("data.db")
 db.setup()
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     executor = futures.ThreadPoolExecutor(4)
     logger.info("Starting checker")
     while True:
-        check = checker.Checker(db, url, timeout=timeout, threads=threads)
+        check = checker.Checker(db, urls, timeout=timeout, threads=threads)
         result = executor.submit(check.start)
         e = result.exception(timeout=None)
         logger.info(e, exc_info=True)
